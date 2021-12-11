1dace97a18215730e389a780ea1c041d1cab37b9
devDaviSousa <devsousadavi@gmail.com>
2021-12-11 13:57:19 -0300

    pesquisa up

--- src/components/index.js
+++ src/components/index.js
@@ -1,33 +1,31 @@
 import React from "react";
 
-
 export default class Pesquisa extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      chave: '',
       pesquisa: ''
     }
 
     this.handleChangeValue = this.handleChangeValue.bind(this);
+    this.quandoClicar = this.quandoClicar.bind(this);
   }
-
-
-
 
   handleChangeValue(event) {
-    const { callback } = this.props;
-    callback(event.target.value);
+    this.setState({ pesquisa: event.target.value });
   }
 
-
+  quandoClicar() {
+    const { callback } = this.props;
+    callback(this.state.pesquisa);
+  }
 
   render() {
     return (
       <form>
         <input type="text" onChange={this.handleChangeValue} />
-        <button type="button" >Pesquisar</button>
+        <button type="button" onClick={this.quandoClicar}>Pesquisar</button>
       </form>
     )
   }
